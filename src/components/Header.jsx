f6f321df4446c0d75ff6e24d056132227646ef58
Jared Roder <jared.roder@merkle.com>
2024-12-04 11:48:20 -0600

    remove unused article and its links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,7 +116,6 @@ function MobileNavigation(props) {
             <MobileNavItem href="/about">About</MobileNavItem>
             <MobileNavItem href="/articles">Articles</MobileNavItem>
             {/*<MobileNavItem href="/projects">Projects</MobileNavItem>*/}
-            {/*<MobileNavItem href="/speaking">Speaking</MobileNavItem>*/}
             <MobileNavItem href="/tools">Tools</MobileNavItem>
           </ul>
         </nav>
@@ -155,7 +154,6 @@ function DesktopNavigation(props) {
         <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         {/*<NavItem href="/projects">Projects</NavItem>*/}
-        {/*<NavItem href="/speaking">Speaking</NavItem>*/}
         <NavItem href="/tools">Tools</NavItem>
       </ul>
     </nav>
